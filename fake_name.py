00f43d67fdc1aa92f124a52ba292f500ceb15159
Andrey Shumik <shumik.by@gmail.com>
2023-07-08 21:18:13 +0300

    порядок колонок и шапка с названиями

--- fake_name.py
+++ fake_name.py
@@ -3,14 +3,19 @@ import openpyxl
 
 
 def generate_name():
-    return RussianNames().get_person().split()
+    a = RussianNames().get_person().split()
+    name = a[1]
+    surname = a[0]
+    patronymic = a[2]
+    return [a[1]]+[a[0]]+[a[2]]
+
     
  
 if __name__ == "__main__":
     number = int(input('Vvedite chislo imen: '))
     book = openpyxl.Workbook() #создание книги эксель
     sheet = book.active
-
+    sheet.append(['Номер','Фамилия','Имя','Отчество'])
     for i in range(number):
         sheet.append([i]+generate_name())
     
