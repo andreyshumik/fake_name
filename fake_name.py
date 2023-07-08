58ceff525f55f43ca0cca024a0270c9c68a67fd9
Andrey Shumik <shumik.by@gmail.com>
2023-07-08 22:18:10 +0300

    добавил функцию и и справил нумерацию

--- fake_name.py
+++ fake_name.py
@@ -4,19 +4,19 @@ import openpyxl
 
 def generate_name():
     a = RussianNames().get_person().split()
-    name = a[1]
-    surname = a[0]
-    patronymic = a[2]
     return [a[1]]+[a[0]]+[a[2]]
+
+def XLSX(number):
+    book = openpyxl.Workbook() #создание книги эксель
+    sheet = book.active
+    sheet.append(['Номер','Фамилия','Имя','Отчество'])
+    for i in range(number):
+        sheet.append([i+1]+generate_name())
+    
+    book.save('555.xlsx')
 
     
  
 if __name__ == "__main__":
     number = int(input('Vvedite chislo imen: '))
-    book = openpyxl.Workbook() #создание книги эксель
-    sheet = book.active
-    sheet.append(['Номер','Фамилия','Имя','Отчество'])
-    for i in range(number):
-        sheet.append([i]+generate_name())
-    
-    book.save('555.xlsx')
+    XLSX(number)
